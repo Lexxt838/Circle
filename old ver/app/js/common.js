4f7f1180ae91a6419a499f68ae2e8769655b4182
lexxt838 <Lexxt838@gmail.com>
2018-01-19 11:31:25 +0200

    тут написать описание о сделанных изменениях

---
+++ old ver/app/js/common.js
@@ -0,0 +1,21 @@
+
+	// $('.multiple-items').slick({
+	//   infinite: true,
+	//   slidesToShow: 3,
+	//   slidesToScroll: 1
+	// });
+	
+
+	$('.autoplay-Lexx').slick({
+		infinite: true,
+		arrows: true,
+		// prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+		// nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+		centerMode: true,
+	  // centerPadding: '100px',
+	  variableWidth: true,
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  // autoplay: true,
+	  autoplaySpeed: 2000,
+	});
